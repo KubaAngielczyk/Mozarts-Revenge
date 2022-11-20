b6e71e4f4462ca794f3b7fa03ec3acad26f4e99f
Calnic1345 <93145984+Calnic1345@users.noreply.github.com>
2022-11-20 20:33:08 +0100

    Bullet impact effects
    to co w tytule

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -6,6 +6,7 @@ public class PlayerBullet : MonoBehaviour
     public float speed = 7.5f;
     public Rigidbody2D theRB;
     public int damageToGive = 15;
+    public GameObject impactEffect;
 
     void Start()
     {
@@ -24,8 +25,15 @@ public class PlayerBullet : MonoBehaviour
         {
 
             other.GetComponent<EnemyController>().DamageEnemy(damageToGive);
+            Instantiate(impactEffect, transform.position, transform.rotation);
             Destroy(gameObject);
         }
         
     }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
 }
