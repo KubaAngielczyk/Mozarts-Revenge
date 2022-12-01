ea239273420629b7c09660384c275b02aff63616
Calnic1345 <93145984+Calnic1345@users.noreply.github.com>
2022-12-01 08:04:12 +0100

    Boss i lekki update poziomów
    xd

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        
+            if (Input.GetKeyDown("f"))
+            {
+            again();
+                
+            }
+        
+    }
+
+    void again()
+    {
+        
+            SceneManager.LoadScene("SampleScene");
+
+        
+    }
+}
